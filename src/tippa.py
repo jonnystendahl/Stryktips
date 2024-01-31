c6813cdd418339e6ab32c3bce792c02ff3c7f024
Jonny Stendahl <jonny.stendahl@skjulet.se>
2024-01-31 20:14:29 +0100

    Fix

--- src/tippa.py
+++ src/tippa.py
@@ -1,27 +1,17 @@
 import numpy as np
 
 class TippaRows:
-    _rows = []
-
+    
     def __init__(self):
-        pass    
+        np_rows = np.empty(1594323, dtype = str)
+        np_odds = np.empty(1594323, dtype = float)
+        np_sorted = np.zeros(1594323, dtype = int)
 
     def create_all_rows(self):
         
-        # for x in range(3):
-        #     row = []
-        #     for y in range(13):
-        #         if x == 0:
-        #             row.append('1')
-        #         elif x == 1:
-        #             row.append('X')
-        #         else:
-        #             row.append('2')
-        #     print('Row {} = {}'.format(x, row))
-        #     print()
-        #     self._rows.append(row)
-
+        rows = []
         signs = '1X2'
+        
         for p1 in signs:
             for p2 in signs:
                 for p3 in signs:
@@ -35,50 +25,29 @@ class TippaRows:
                                                 for p11 in signs:
                                                     for p12 in signs:
                                                         for p13 in signs:
-                                                            row = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13]
-                                                            self._rows.append(str(row))
+                                                            rows.append(''.join([p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13]))
+        self.np_rows = np.array(rows)
+        rng = np.random.default_rng()
+        self.np_odds = rng.integers(low = 4, high = 10, size = 1594323)
+        self.np_sorted = np.argsort(self.np_odds)
                     
-
-    
-    def add_row(self, row, odds):
-        newrow = {}
-        newrow["row"] = row
-        newrow["odds"] = odds
-        self._rows.append(newrow)
-
-    def sort_rows(self):
-         self._rows = sorted(self._rows, key=lambda d: d['odds'])
-
     def print_rows(self):
-         for row in self._rows:
-            print(row)
+        print("Min = ", np.min(self.np_odds))
+        print(self.np_odds[0:4])
+        print(self.np_sorted[0:4])
+        print(self.np_odds[self.np_sorted[0]])
+        print("END")
 
     def set_odds(self, index, odds):
-        self._rows[index - 1]["odds"] = odds
-
-    def create_numpy_array(self):
-        arr = np.array(self._rows)
-        print(arr)
-
+        self.np_odds[index] = odds
 
 def main():
+    import time
+    
     Tippa = TippaRows()
 
     Tippa.create_all_rows()
-
-    # Tippa.add_row("XXX", 2)
-    # Tippa.add_row("111", 1)
-    # Tippa.add_row("222", 99)
-    
-    # Tippa.sort_rows()
-    # print("Original")
-    # Tippa.print_rows()
-    # Tippa.set_odds(1, 77)
-    # Tippa.sort_rows()
-    # print("Changed")
-    # Tippa.print_rows()
-    Tippa.create_numpy_array()
-    
+    Tippa.print_rows()
 
 if __name__ == "__main__":
     main()
